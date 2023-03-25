ad6e1cd770097ce58895933e8ad0c2bca8be46c2
TMSadique <110740296+TMSadique@users.noreply.github.com>
2023-03-25 10:48:23 +0530

    rollercoaster_ticker.py

---
+++ rollercoaster_ticker.py
@@ -0,0 +1,31 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cms? "))
+
+bill = 0
+
+if height >= 120:
+  age = int(input("What is your age? "))
+
+  if age < 12:
+    bill = 5
+    print(f"Child tickets are ${bill}.")
+
+  elif age <18:
+    bill = 7
+    print(f"Youth tickets are ${bill}.")
+
+  else:
+    bill = 12
+    print(f"Adult tickets are ${bill}.")
+
+  photo = input("Do you wanty to take a photo? Y or N. ")
+  if photo == "Y":
+    bill += 3                                                  #Add $3 to bill.
+    print(f"Your final bill is ${bill}")
+    print(f"Enjoy your ride!")
+
+  else:
+    print(f"Enjoy your ride!")
+    
+else:
+  print("Sorry, You have to grow taller before you can ride! :( ")
